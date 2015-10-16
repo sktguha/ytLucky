1eff3687dc7788a78f0c56a59abce960df8bc567
Saikat Guha <sktguha@gmail.com>
2015-10-16 16:10:48 +0530

    removed the bug if net disconnected when extension starting
    up then the gapi client is not inititalised. Now it is retried with
    increasing logarthmic intervals, max set at 5 mins

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+    var KEY = "AIzaSyCbZXtN7-rCDxlVdmpsv7BBhjYKzGYfrkE";
+    var getInterval = getIntervalFunction();
+    attachScript();
+    function attachScript(){
+        var sc = document.getElementById('script');
+        if(sc){
+            sc.onerror = null;
+            sc.parentElement.removeChild(sc);
+        }
+        sc = document.createElement("script");
+        sc.src = "https://apis.google.com/js/client.js?onload=onClientLoad";
+        sc.id = "script";
+        document.body.appendChild(sc);
+        sc.onerror = function(){
+            setTimeout(attachScript, getInterval());
+        }
+    }
+
     function onClientLoad() {
         gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
     }
@@ -26,3 +44,35 @@
         // if sendResponse is called async then true needs to be returned, else sendResponse becomes invalid, http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
         return true;
     });
+
+    function startsWith(str, ss){
+        return str.substring(0, ss.length) === ss;
+    }
+    
+    function getVideoId(items){
+        for(var i = 0; i< items.length; i++){
+            if(items[i].id && items[i].id.videoId){
+                return items[i].id.videoId;
+            }
+        }
+        return false;
+    }
+
+    function getIntervalFunction(retryLimit, minInterval, maxInterval) {
+        var HIGH_INTERVAL = maxInterval || 300000,
+            LOW_INTERVAL = (typeof minInterval == "undefined") ? 4000 : maxInterval,
+            RETRY_LIMIT = retryLimit || 10,
+            count = 0,
+            interval = 0;
+        return function () {
+            if(count > RETRY_LIMIT){
+                return -1;
+            }
+            interval = LOW_INTERVAL*Math.pow(2, count);
+            count++;
+            if(interval > HIGH_INTERVAL){
+                return HIGH_INTERVAL;
+            }
+            return interval;
+        }
+    }
