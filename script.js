894d7dad3e6e0d7aca54aa262c789bc55a090f01
Saikat Guha <sktguha@gmail.com>
2018-08-26 03:27:22 +0530

    run at window.onload and if target button not found try again after 2 secs for max of 15 times

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 var inputSearch;
+function maxTimes(fn,n, ctx){
+	var lastRes;
+	return function(){
+		if(n === 0) return lastRes;
+		n--;
+		lastRes = fn.apply(ctx || this, arguments);
+		return lastRes;
+	}
+}
+var setTimeout15 = maxTimes(window.setTimeout, 15, window);
 function init() {
-    var button = document.getElementById('search-btn');
-    var lButton = document.createElement('button');
-    
+    var button = document.getElementById('search-icon-legacy');
+    if(!button) { 
+		setTimeout15(init, 2000); 
+		return;
+	}
+	var lButton = document.createElement('button');
     lButton.className = button.className;
     lButton.dir = button.dir;
-    lButton.innerHTML = "I'm feeling lucky!";
+    lButton.innerHTML = "I'm feeling lucky!";	
     button.parentNode.insertBefore(lButton, button);
     lButton.style.padding = '2px 2px 2px 2px';
     lButton.onclick = handleClick;
@@ -17,7 +30,7 @@ function init() {
         }
     }
 }
-init();
+window.onload = init;
 function handleClick(e){
 	if(!inputSearch){
 		console.error('cannot find search input element');
